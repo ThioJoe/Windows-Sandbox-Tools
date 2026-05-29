d6aaf290dea939a8e51b0488c21b0faf1303c05e
ThioJoe <12518330+ThioJoe@users.noreply.github.com>
2026-05-29 16:19:00 -0700

    Startup Script: Enable taskbar End Task option

--- Startup Scripts/SandboxStartup.ps1
+++ Startup Scripts/SandboxStartup.ps1
@@ -31,6 +31,9 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "H
 # Enable Windows Long Path support
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\FileSystem" /v "LongPathsEnabled" /t REG_DWORD /d 1 /f
 
+# Add End Task option to Taskbar right click menu
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\TaskbarDeveloperSettings" /v "TaskbarEndTask" /t REG_DWORD /d 1 /f
+
 # Fix for slow MSI package install. Note: On a normal machine this would disable "Smart App Control", it's unknown the exact effect within the sandbox. It likely disables some kind of SmartScreen security checking.
 # See: https://github.com/microsoft/Windows-Sandbox/issues/68#issuecomment-2754867968
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\CI\Policy" /v "VerifiedAndReputablePolicyState" /t REG_DWORD /d 0 /f
